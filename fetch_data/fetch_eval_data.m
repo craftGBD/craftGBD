5c380c3a56fb4d82718a9bb17994d8d0f230e963
craftGBD <yanjunjie@outlook.com>
2016-10-09 12:06:30 +0800

    Update fetch_eval_data.m

--- fetch_data/fetch_eval_data.m
+++ fetch_data/fetch_eval_data.m
@@ -3,7 +3,7 @@ cd(fileparts(mfilename('fullpath')));
 
 try
     fprintf('Downloading eval data...\n');
-    urlwrite('https://onedrive.live.com/download?cid=2D6AAB50CBDC610F&resid=2D6AAB50CBDC610F%211461&authkey=APnggEjkWW1cPEw', ...
+    urlwrite('https://onedrive.live.com/download?cid=2D6AAB50CBDC610F&resid=2D6AAB50CBDC610F%211472&authkey=ANC-D8F_bKPKxy4', ...
         'data.zip');
     fprintf('Unzipping...\n');
     unzip('data.zip', '../evaluation');
@@ -15,7 +15,7 @@ end
 
 try
     fprintf('Downloading eval devkit...\n');
-    urlwrite('https://onedrive.live.com/download?cid=2D6AAB50CBDC610F&resid=2D6AAB50CBDC610F%211461&authkey=APnggEjkWW1cPEw', ...
+    urlwrite('https://onedrive.live.com/download?cid=562BEF628FB22588&resid=562BEF628FB22588%21573&authkey=AExuFXM8W3IKLpk', ...
         'devkit.zip');
     fprintf('Unzipping...\n');
     unzip('devkit.zip', '../evaluation');
